14083bff2256f8a22fdad61a1a79fbab3411ad4a
Vic Ong <vickyocc53@gmail.com>
2019-03-15 19:48:32 -1000

    deploy demo app

---
+++ precache-manifest.bf07c146f99ef0c872df97db1db3b70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8f27680fd471d028b0f1",
+    "url": "/js/chunk-vendors.0abfcecf.js"
+  },
+  {
+    "revision": "22804ce18021a82004b4",
+    "url": "/js/app.a488caab.js"
+  },
+  {
+    "revision": "f7356823a2e3e9c73814ddc54b8cc1f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f27680fd471d028b0f1",
+    "url": "/css/chunk-vendors.e751b5aa.css"
+  },
+  {
+    "revision": "22804ce18021a82004b4",
+    "url": "/css/app.4293cf13.css"
+  }
+];
